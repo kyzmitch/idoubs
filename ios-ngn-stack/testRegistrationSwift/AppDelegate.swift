88bd11b30f3cdab67d86c0670ae28506c4f041eb
Andrei Ermoshin <iermoshin@gmail.com>
2018-08-07 15:30:21 +0300

    Initialization  of NgnEngine in swift

--- ios-ngn-stack/testRegistrationSwift/AppDelegate.swift
+++ ios-ngn-stack/testRegistrationSwift/AppDelegate.swift
@@ -13,10 +13,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    var mEngine: NgnEngine?
+    var mEngine: NgnEngine!
+    var mSipService: (NgnBaseService & INgnSipService)!
+    var mConfigurationService: (NgnBaseService & INgnConfigurationService)!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
+        mEngine = NgnEngine.sharedInstance()
+        mEngine.start()
+        mSipService = mEngine.sipService
+        mConfigurationService = mEngine.configurationService
 
         return true
     }
